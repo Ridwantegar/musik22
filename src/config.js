68fdd27e272d55df2afb024e2c5ffb21c6883fe6
unknown <tegarridwqan82@gmail.com>
2023-08-14 13:05:27 +0700

    update

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   token: process.env.TOKEN || 'MTEzOTg4Nzc2Mzc2OTAwMDAwNg.GrdIx1.qIaUNORMymiD0foz5OMIneoTJaZ39jB-09ieOo', // your discord bot token
-  prefix: process.env.PREFIX || 'VZ!', // bot prefix
+  prefix: process.env.PREFIX || 'KTL!', // bot prefix
   ownerID: process.env.OWNERID?.split(',') || ['1125031170422345738','337663518952390656',``], //your discord id
   SpotifyID: process.env.SPOTIFYID || '2d340b0e4f6143d0b908f7061a2474d7', // spotify client id
   SpotifySecret: process.env.SPOTIFYSECRET || 'adb0862e14a7403ab904b360a33ca49a', // spotify client secret
